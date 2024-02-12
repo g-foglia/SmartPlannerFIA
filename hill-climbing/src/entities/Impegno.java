ef37cf76193c15381bcfedc3aab9020b45341750
isaboutoftown <vivispillthetea@gmail.com>
2024-02-12 16:29:10 +0100

    Modificata classe impegno con rimozione oggetti non utili

--- hill-climbing/src/entities/Impegno.java
+++ hill-climbing/src/entities/Impegno.java
@@ -1,29 +1,21 @@
 package entities;
 
 public class Impegno {
-    private int codiceImpegno;
+
     private String nomeImpegno;
-    private int durataImpegno;
     private int prioritaImpegno;
 
 
 
 //metodo costruttore
-    public Impegno(String nomeImpegno, int durataImpegno, int prioritaImpegno) {
+    public Impegno(String nomeImpegno,  int prioritaImpegno) {
         this.nomeImpegno = nomeImpegno;
-        this.durataImpegno = durataImpegno;
         this.prioritaImpegno = prioritaImpegno;
+    }
 
-    }
-//metodo per restituire il codice dell'impegno
-    public int getCodiceImpegno() {
-        return codiceImpegno;
-    }
 //metodo per settare il codice dell'impegno
 
-    public void setCodiceImpegno(int codiceImpegno) {
-        this.codiceImpegno = codiceImpegno;
-    }
+
 //metodo per restituire il nome dell'impegno
 
     public String getNomeImpegno() {
@@ -33,14 +25,7 @@ public class Impegno {
     public void setNomeImpegno(String nomeImpegno) {
         this.nomeImpegno = nomeImpegno;
     }
-    //metodo per restituire la durata dell'impegno
-    public int getDurataImpegno() {
-        return durataImpegno;
-    }
-    //metodo per settare la durata dell'impegno
-    public void setDurataImpegno(int durataImpegno) {
-        this.durataImpegno = durataImpegno;
-    }
+
     //metodo per restituire la priorità  dell'impegno
     public int getPrioritaImpegno() {
         return prioritaImpegno;
